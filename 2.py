25fc74766037cb5add4a6b76cd338928c24d3357
Max <7enidomaks@gmail.com>
2023-10-24 15:33:43 +0300

    Выполнение 2 задания

--- 2.py
+++ 2.py
@@ -1,17 +1,17 @@
-number_of_rep=input("")
-pattern = [[1, 1, 1, 1, 0, 0],
-           [1, 0, 0, 1, 0, 0],
-           [1, 0, 1, 1, 0, 0],
-           [1, 0, 1, 0, 0, 0],
-           [1, 0, 1, 1, 1, 1]]
-for i in range(int(number_of_rep)):
-    for string in range(5):
-        for column in range(6):
-            if pattern[string][column] == 1:
-                print("\x1b[31m",end='')
-                print("#",end='')
-            else:
-                print("\x1b[39m",end='')
-                print("#",end='')
-        print("\t")
-
+number_of_rep = int(input("Введите количество повторений узора:"))
+SIZE_OF_PATTERN_X = 5
+SIZE_OF_PATTERN_Y = 6*number_of_rep
+pattern = [[1, 1, 1, 1, 0, 0]*number_of_rep,
+           [1, 0, 0, 1, 0, 0]*number_of_rep,
+           [1, 0, 1, 1, 0, 0]*number_of_rep,
+           [1, 0, 1, 0, 0, 0]*number_of_rep,
+           [1, 0, 1, 1, 1, 1]*number_of_rep]
+for string in range(SIZE_OF_PATTERN_X):
+    for column in range(SIZE_OF_PATTERN_Y):
+        if pattern[string][column] == 1:
+            print("\x1b[31m", end='')
+            print("#", end='')
+        else:
+            print("\x1b[39m", end='')
+            print("#", end='')
+    print("\t")
